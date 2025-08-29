87d897892844adefc9eb92f15acd2753c7df5436
Ronit Kundnani <ronitkundnani08@gmail.com>
2025-08-29 12:18:52 +0530

    paranthesis

--- Exp3/3.c
+++ Exp3/3.c
@@ -1,90 +1,119 @@
-#include<stdio.h>
-#include<string.h>
-#define MAX 20
-
-struct stack{
-    char s[MAX];
+#include <stdio.h>
+#include <stdlib.h>
+struct stack
+{
+    int size;
     int top;
+    char *arr;
 };
 
-struct stack s1;
+int isEmpty(struct stack *s)
+{
+    return s->top == -1;
+}
 
-int isEmpty(){
-    if(s1.top == -1){
-        printf("Stack is Empty or Underflow\n");
+int isFull(struct stack *s)
+{
+    return s->top == s->size - 1;
+}
+
+char push(struct stack *s, char value)
+{
+    if (isFull(s))
+    {
+        printf("Stack overflow\n");
+        return 0;
+    }
+    else
+    {
+        s->top++;
+        s->arr[s->top] = value;
+        return value;
+    }
+}
+
+char pop(struct stack *s)
+{
+    if (isEmpty(s))
+    {
+        printf("Stack underflown\n");
+        return 0;
+    }
+    else
+    {
+        return s->arr[s->top--];
+    }
+}
+
+int match(char f, char b)
+{
+    if (f == '{' && b == '}')
+    {
+        return 1;
+    }
+    if (f == '[' && b == ']')
+    {
+        return 1;
+    }
+    if (f == '(' && b == ')')
+    {
         return 1;
     }
     return 0;
 }
 
-int isFull(){
-    if(s1.top == MAX-1){
-        printf("Stack is Full or Overflow\n");
-        return 1;
-    }
-    return 0;
-}
+int parenthesisMatch(char *exp)
+{
+    struct stack *sp;
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+    char popped_ch;
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+        {
+            push(sp, exp[i]);
+        }
+        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+        {
+            if (isEmpty(sp))
+            {
+                return 0;
+            }
 
-void push(char ele){
-    if(isFull()){
-       printf("You can't push an element\n");
-    }else{
-        s1.top += 1;
-            s1.s[s1.top]=ele;
-            
-    }
-}
-
-char pop(){
-    if(isEmpty()){
-       printf("You can't pop an element\n");
-    }else{
-        char popped = s1.s[s1.top];
-        
-        s1.top -= 1;
-        return popped;
-    }
-}
-
-void Top(){
-    printf("Top element of stack is %c\n" ,s1.s[s1.top]);
-
-}
-
-int isValid(char* s) {
-    int i;
-    for (i = 0; s[i] != '\0'; i++) {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-            push(s[i]);
-        } else if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
-            if (isEmpty()) return 0; 
-            char top = pop();
-            if ((s[i] == ')' && top != '(') ||
-                (s[i] == '}' && top != '{') ||
-                (s[i] == ']' && top != '[')) {
-                return 0; 
+            popped_ch = pop(sp);
+            if (!match(popped_ch, exp[i]))
+            {
+                return 0;
             }
         }
     }
-    return isEmpty(); 
+    if (isEmpty(sp))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-
-
-int main(){
+int main()
+{
     printf("Student Name: Ronit Kundnani\n");
     printf("Student RollNo: 24BIT100\n");
-    s1.top = -1;
-    
-    char A[MAX];
-    printf("Enter a string of parentheses: ");
-    fgets(A, MAX, stdin);
-    A[strcspn(A, "\n")] = 0; 
-    if (isValid(A)) {
-        printf("The string is valid.\n");
-    } else {
-        printf("The string is invalid.\n");
+
+    char *exp = "[(	)]{}{[(	)(	)](	)}";
+
+    if (parenthesisMatch(exp))
+    {
+        printf("Parenthesis Matching\n");
     }
-    Top();
+    else
+    {
+        printf("Parenthesis not Matching \n");
+    }
+
     return 0;
 }
--- Exp5/1.c
+++ Exp5/1.c
@@ -1,5 +1,101 @@
 #include <stdio.h>
-int main(){
+
+struct Queue
+{
+    int front;
+    int rear;
+    int arr[100];
+};
+
+int isFull(struct Queue *queue){
+    if (queue->rear==99)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
     
+}
+
+int isEmpty(struct Queue *queue){
+    if (queue->front==-1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+
+int insertion(struct Queue *queue,int value){
+    if (isFull(queue))
+    {
+        printf("Queue is Full\n");
+        return 0;
+    }
+    else{
+        queue->rear++;
+        if (queue->front==-1)
+        {
+            queue->front=0;
+        }
+        
+        queue->arr[queue->rear]=value;
+        return value;
+    }
+    
+}
+
+int deletion(struct Queue *queue){
+    if (isEmpty(queue))
+    {
+        printf("Queue is Empty\n");
+        return 0;
+    }
+    else{
+        int value=queue->arr[queue->rear];
+        queue->rear--;
+        return value;
+    }
+}
+
+int front(struct Queue *queue){
+    if (isEmpty(queue))
+    {
+        printf("Queue is empty\n");
+        return 0;
+    }
+    else{
+        return queue->arr[queue->front];
+    }
+}
+
+int rear(struct Queue *queue){
+    if (!isEmpty(queue))
+    {
+        return queue->arr[queue->rear];
+    }
+    else{
+        printf("Queue is empty\n");
+        return 0;
+    }
+}
+
+int main()
+{
+    struct Queue queue;
+    queue.front=-1;
+    queue.rear=-1;
+    insertion(&queue,10);
+    printf("%d\n",front(&queue));
+    printf("Value inserted %d\n",insertion(&queue,20));
+    printf("Value inserted %d\n",insertion(&queue,40));
+    printf("Value inserted %d\n",insertion(&queue,39));
+    printf("Value inserted %d\n",insertion(&queue,30));
+    printf("Value deleted %d\n",deletion(&queue));
+    printf("%d\n",front(&queue));
+    printf("%d\n",rear(&queue));
     return 0;
 }
