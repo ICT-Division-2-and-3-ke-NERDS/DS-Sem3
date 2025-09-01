fe316b6f66f6eca2567838e4899b37bb21aba779
Ronit Kundnani <ronitkundnani08@gmail.com>
2025-09-01 23:09:01 +0530

    Error fixed

--- Exp4/1.c
+++ Exp4/1.c
@@ -1,109 +1,93 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-struct stack
-{
+
+struct stack {
     int size;
     int top;
     char *arr;
 };
 
-int isEmpty(struct stack *s)
-{
+int isEmpty(struct stack *s) {
     return s->top == -1;
 }
 
-int isFull(struct stack *s)
-{
+int isFull(struct stack *s) {
     return s->top == s->size - 1;
 }
 
-char pop(struct stack *s)
-{
-    if (isEmpty(s))
-    {
+char pop(struct stack *s) {
+    if (isEmpty(s)) {
         printf("stack underflow\n");
         return 0;
     }
-    else
-    {
-        return s->arr[s->top--];
-    }
+    return s->arr[s->top--];
 }
 
-char push(struct stack *s, char value)
-{
-    if (isFull(s))
-    {
+char push(struct stack *s, char value) {
+    if (isFull(s)) {
         printf("stack overflow\n");
         return -1;
     }
-    else
-    {
-        s->top++;
-        s->arr[s->top] = value;
-        return value;
-    }
+    s->arr[++s->top] = value;
+    return value;
 }
 
-char top(struct stack *s)
-{
+char top(struct stack *s) {
+    if (isEmpty(s)) return '\0';
     return s->arr[s->top];
 }
 
-int isOperator(char a)
-{
-    if (a == '+' || a == '-' || a == '/' || a == '*')
-        return 1;
-    else
-        return 0;
+int isOperator(char a) {
+    return (a == '+' || a == '-' || a == '*' || a == '/');
 }
 
-int precedence(char a)
-{
+int precedence(char a) {
     if (a == '*' || a == '/')
         return 3;
     else if (a == '+' || a == '-')
         return 2;
+    else if (a == '(')
+        return 1; // lowest precedence
     else
         return 0;
 }
 
-char *inftopost(char *infix)
-{
+char *inftopost(char *infix) {
     struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
     sp->size = 100;
     sp->top = -1;
     sp->arr = (char *)malloc(sp->size * sizeof(char));
+
     char *postfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
-    int i = 0; // infix traversal
-    int j = 0; // postfix traversal
-    while (infix[i] != '\0')
-    {
-        if (!isOperator(infix[i]))
-        {
-            postfix[j] = infix[i];
+    int i = 0, j = 0;
+
+    while (infix[i] != '\0') {
+        if (infix[i] == '(') {
+            push(sp, infix[i]);
             i++;
-            j++;
         }
-        else
-        {
-            if (precedence(infix[i]) > precedence(top(sp)))
-            {
-                push(sp, infix[i]);
-                i++;
+        else if (infix[i] == ')') {
+            while (!isEmpty(sp) && top(sp) != '(') {
+                postfix[j++] = pop(sp);
             }
-            else
-            {
-                postfix[j] = pop(sp);
-                j++;
+            pop(sp); // remove '('
+            i++;
+        }
+        else if (!isOperator(infix[i])) {
+            postfix[j++] = infix[i++];
+        }
+        else {
+            while (!isEmpty(sp) && precedence(top(sp)) >= precedence(infix[i])) {
+                postfix[j++] = pop(sp);
             }
+            push(sp, infix[i]);
+            i++;
         }
     }
-    while (!isEmpty(sp))
-    {
-        postfix[j] = pop(sp);
-        j++;
+
+    while (!isEmpty(sp)) {
+        postfix[j++] = pop(sp);
     }
     postfix[j] = '\0';
     return postfix;
